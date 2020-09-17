daf18a865f47389ae8bc4b335ece26e0b7bd466b
Zachary <zp.lambda@yahoo.com>
2020-09-17 18:22:33 -0400

    ...

--- usermodel-initial/src/main/java/com/lambdaschool/usermodel/models/User.java
+++ usermodel-initial/src/main/java/com/lambdaschool/usermodel/models/User.java
@@ -2,6 +2,8 @@ package com.lambdaschool.usermodel.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -176,6 +178,12 @@ public class User
      */
     public void setPassword(String password)
     {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setNoEncodePassword(String password)
+    {
         this.password = password;
     }
 
@@ -217,5 +225,18 @@ public class User
     public void setRoles(Set<UserRoles> roles)
     {
         this.roles = roles;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthority()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+        for (UserRoles r : this.roles)
+        {
+            String myRole = "ROLE_" +r.getRole().getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+
+        return rtnList;
     }
 }
